dbf1793b871091c501e68b643ab7e43541c327fa
Pat <gameofpat101@gmail.com>
2021-02-05 10:25:30 -0500

    Login Script functions
    -wrote a login script that will check the login information is correct
    --TODO will need to implement the script with the database when we get that working
    -added a pop-up prompt if the login credentials is incorrect
    -added a "Login" Button
    -linked the login button to the proper function
    -added a "sign-up button

---
+++ Parkey/Parkey/LoginProcedures/Login.swift
@@ -0,0 +1,17 @@
+//
+//  Login.swift
+//  Parkey
+//
+//  Created by Patrick Sammon on 2/5/21.
+//  Copyright © 2021 iona. All rights reserved.
+//
+
+import Foundation
+
+
+/**
+    Ideally this will be the script that will contain the procudures
+ */
+func Login(){
+    
+}
