d15fcab9d6dfe64b60ed93c5039a4efd4ecf5f22
Pat <gameofpat101@gmail.com>
2021-02-08 10:15:32 -0500

    Signup function update 2
    -merged with Jose's database work
    -made the password entries as a type "passwrd" so they are hidden for signup/login
    -worked on the "signup" methods
    -checked the credentials of the users password when trying to signup
    -added the parkey logo to a few places
    - Problem with build due to unknown location of "logowords.png", but will work through the next

--- Parkey/Parkey/Views/LoginController.swift
+++ Parkey/Parkey/Views/LoginController.swift
@@ -28,14 +28,17 @@ class ViewController: UIViewController {
 
     @IBAction func testBtnPressed(_ sender: Any)
     {
+        //grab the URL for the database (currently set to local)
         let url = URL(string: "http://127.0.0.1:8080/newUser")!
-        
+        //create the encoder that will be used
         let encoder = JSONEncoder()
         
+        //make a POST request to the database so it is more secure
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         let newUser = User(name: EmailLogin.text!, age: Int(PasswordLogin.text!)!)
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        //try to encode the body with the user data
         request.httpBody = try? encoder.encode(newUser)
         URLSession.shared.dataTask(with: request) {data, response, error in
             if let data = data
@@ -72,7 +75,7 @@ class ViewController: UIViewController {
             {
                 DispatchQueue.main.sync { [self] in
                     self.users = users
-                    showData()
+                    self.showData()
                     
                 }
             } else
--- Parkey/Parkey/Views/SignUpController.swift
+++ Parkey/Parkey/Views/SignUpController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class SignUpController: UIViewController {
 
+    //Outlet variables
+    @IBOutlet weak var FName: UITextField!
+    @IBOutlet weak var LName: UITextField!
+    @IBOutlet weak var Email: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    @IBOutlet weak var Password2: UITextField!
+    @IBOutlet weak var ErrorMessage: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -50,5 +59,29 @@ class SignUpController: UIViewController {
         } else {
           return false
         }
+    }
+    
+    //method that will be called when the user tries to sign-up
+    @IBAction func SignUp(_ sender: Any) {
+        //check that the email is not in use
+        
+        //check that the password is the same for both entries
+        if Password.text != Password2.text{
+            //then prompt the user to reenter the password
+            ErrorMessage.text = "Passwords do not match"
+            ErrorMessage.isHidden=false
+            return
+        }
+        
+        //check that the password contains the necessary elements
+        let passwordTemp = Password.text!
+        if !checkNumber(password: passwordTemp) || !checkSymbol(password: passwordTemp) || !checkUpperCase(password: passwordTemp){
+            //tell the user they must contain the proper format
+            ErrorMessage.text = "Password must include a number, special character, and uppercase"
+            ErrorMessage.isHidden=false
+            return
+        }
+        
+        //then add the entry to the database
     }
 }
