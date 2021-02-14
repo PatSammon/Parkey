bbad068ff74f93ed1352522f5c7cdbd666b07ad1
Brijian <bleveable@gmail.com>
2021-02-14 13:43:38 -0800

    Account and Setting prototypes added

---
+++ Parkey/Parkey/Code/Views/ProfileController.swift
@@ -0,0 +1,37 @@
+//
+//  ProfileController.swift
+//  Parkey
+//
+//  Created by user188405 on 2/14/21.
+//  Copyright © 2021 iona. All rights reserved.
+//
+
+import UIKit
+
+class ProfileController: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var accountButton: UIButton!
+    
+    @IBOutlet weak var keychainButton: UIButton!
+    
+    @IBOutlet weak var rankingButton: UIButton!
+    
+    @IBOutlet weak var micButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
