f54b6c59a54659428ce5e55713fdc69b69e61a23
Pat <gameofpat101@gmail.com>
2021-02-23 15:48:42 -0500

    making password = *** and auto-sign in
    -obfuscated information with passwords
    -made it so intial screen with automatically go to home screen if user logged in previously
    -added a controller for the initial screen to allow for the auto-sign in

---
+++ Parkey/Parkey/Code/Views/InitalScreenController.swift
@@ -0,0 +1,25 @@
+//
+//  InitalScreenController.swift
+//  Parkey
+//
+//  Created by Patrick Sammon on 2/23/21.
+//  Copyright © 2021 iona. All rights reserved.
+//
+
+import UIKit
+
+class InitalScreenController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // check if it should auto-sign in
+        if UserDefaults.standard.bool(forKey: "LoggedIn"){
+            goToDifferentView()
+        }
+    }
+    
+    @objc func goToDifferentView(){
+        self.performSegue(withIdentifier: "AutoLoginSegue", sender: self)
+    }
+}
